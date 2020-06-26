973919e9d104e157b287f16669efc944e9baec68
reza <gholamzadreza@gmail.com>
2020-06-26 06:10:43 -0700

    add scheduled method

---
+++ src/main/java/com/prospringboot/messagingRabbitMQ/rmq/ToDoSender.java
@@ -0,0 +1,46 @@
+package com.prospringboot.messagingRabbitMQ.rmq;
+
+import com.prospringboot.messagingRabbitMQ.domain.ToDo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+    @EnableScheduling. This annotation tells (via auto-configuration)
+    the Spring container that the org.springframework.scheduling.
+    annotation.ScheduledAnnotationBeanPostProcessor class
+    needs to be created. It registers all the methods annotated
+    with @Scheduled to be invoked by an org.springframework.
+    scheduling.TaskScheduler interface implementation according
+    to the fixedRate, fixedDe
+ */
+@EnableScheduling
+@Configuration
+public class ToDoSender {
+
+    private final ToDoProducer toDoProducer;
+
+    public ToDoSender(ToDoProducer toDoProducer) {
+        this.toDoProducer = toDoProducer;
+    }
+
+    @Value("${todo.amqp.queue}")
+    private String destination;
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    /*
+        @Scheduled(fixedDelay = 500L). This annotation tells the
+        TaskScheduler interface implementation to execute the sendToDos
+        method with a fixed delay of 500 milliseconds. This means that every
+        half second you send a message to the queue.
+     */
+    @Scheduled(fixedRate = 500L)
+    private void sendTo(){
+        toDoProducer.sendTo(destination,
+                new ToDo("Thinking on Spring Boot at " + dateFormat.format(new Date())));
+    }
+}
